fa3784910058b7dc710fd0d46e1f8bc61383e302
Patrick <patrykrzucidlo1@gmail.com>
2023-08-21 23:07:41 +0200

    feat: list public worlds

--- services/node/src/application/src/world/world_service.rs
+++ services/node/src/application/src/world/world_service.rs
@@ -17,6 +17,10 @@ impl WorldService {
         Self { world_repository }
     }
 
+    pub async fn get_public_worlds(&self) -> Result<Vec<World>, StorageError> {
+        self.world_repository.find_all_public().await
+    }
+
     pub async fn create_world(
         &self,
         name: &str,
